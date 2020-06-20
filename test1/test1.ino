acc9d8647ffe731a6cd8b536d449d6f092a56a39
DylanPJackson <frprdjackson@gmail.com>
2020-06-20 10:50:38 -0500

    Functional non-trivial test

--- test1/test1.ino
+++ test1/test1.ino
@@ -13,7 +13,9 @@ RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
 void setup() {
     matrix.begin();
-    matrix.drawPixel(0,0, matrix.Color333(7,7,7));
+    matrix.fillRect(0,0,11,32, matrix.Color333(0,0,1));
+    matrix.fillRect(11,0,11,32, matrix.Color333(1,1,1));
+    matrix.fillRect(22,0,10,32, matrix.Color333(1,0,0));
 }
 
 void loop() {
